f265db4a6c3f151c061059f36faf6b6e7dd1011f
Anthony Delbee <anthony.delbee@gmail.com>
2025-09-03 17:36:59 +0200

    ajout toggle activation API

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class UserController extends AbstractController
 {
@@ -16,19 +15,29 @@ final class UserController extends AbstractController
     public function showMyAccount(OrderRepository $orderRepository): Response
     {
         // @todo ajouter is granted pour géré l'accès
-//        $orders = $orderRepository->findBy(['user' => $this->getUser()]);
+        $user = $this->getUser();
+        $orders = $orderRepository->findBy(['owner' => $user]);
 
         return $this->render('user/account.html.twig', [
-            'orders' => [],
+            'orders' => $orders,
+            'user' => $user,
         ]);
     }
 
     #[Route('/my_account/api_authorize', name: 'app_account_api_authorize')]
-    public function apiAuthorize(UserRepository $userRepository,EntityManagerInterface $em): Response
+    public function apiAuthorize(EntityManagerInterface $em): Response
     {
         // @todo ajouter is granted pour géré l'accès
-        $user = $userRepository->findOneBy(['user' => $this->getUser()]);
-        $user->setIsApiAuthorized(true);
+
+        $user = $this->getUser();
+
+        // toggle bool isApiAuthorized
+        if ($user->isApiAuthorized() === false) {
+            $user->setIsApiAuthorized(true);
+        }else {
+            $user->setIsApiAuthorized(false);
+        }
+
         $em->persist($user);
         $em->flush();
 
